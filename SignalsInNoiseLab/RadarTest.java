e116080e84dd37213ac6c1306609185c439d7048
pgbeckman <pgbeckman@student.naperville203.org>
2014-12-13 13:51:15 -0600

    Test Added

---
+++ SignalsInNoiseLab/RadarTest.java
@@ -0,0 +1,40 @@
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class RadarTest.
+ *
+ * @ Paul Beckman
+ * @ 12/13/14
+ */
+public class RadarTest
+{
+    /**
+     * Default constructor for test class RadarTest
+     */
+    public RadarTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
