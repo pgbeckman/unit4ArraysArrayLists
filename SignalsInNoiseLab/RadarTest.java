c020a3ae7bf3e67291ab97d0ff1d6addbaf07b37
pgbeckman <pgbeckman@student.naperville203.org>
2014-12-15 11:12:14 -0600

    LabFinal

--- SignalsInNoiseLab/RadarTest.java
+++ SignalsInNoiseLab/RadarTest.java
@@ -37,4 +37,55 @@ public class RadarTest
     public void tearDown()
     {
     }
+    
+    /**
+     * Velocity [1,1] with low noise
+     */
+    @Test
+    public void test1(){
+        Radar radar = new Radar(100, 100);
+        radar.setNoiseFraction(.0001);
+        radar.setMonsterLocation(50,50,1,1);
+        radar.scan();
+        for(int i = 0; i < 100; i++)
+        {
+            radar.scan();
+        }
+        assertEquals(1,radar.getVelocity()[0],1e-6);
+        assertEquals(1,radar.getVelocity()[1],1e-6);
+    }
+    
+    /**
+     * Velocity [-3,4] with medium noise
+     */
+    @Test
+    public void test2(){
+        Radar radar = new Radar(100, 100);
+        radar.setNoiseFraction(.001);
+        radar.setMonsterLocation(30,70,-3,4);
+        radar.scan();
+        for(int i = 0; i < 100; i++)
+        {
+            radar.scan();
+        }
+        assertEquals(-3,radar.getVelocity()[0],1e-6);
+        assertEquals(4,radar.getVelocity()[1],1e-6);
+    }
+    
+    /**
+     * Velocity [2,-5] with med-high noise
+     */
+    @Test
+    public void test3(){
+        Radar radar = new Radar(100, 100);
+        radar.setNoiseFraction(.01);
+        radar.setMonsterLocation(80,10,2,-5);
+        radar.scan();
+        for(int i = 0; i < 100; i++)
+        {
+            radar.scan();
+        }
+        assertEquals(2,radar.getVelocity()[0],1e-6);
+        assertEquals(-5,radar.getVelocity()[1],1e-6);
+    }
 }
